b086eb4bb18c474609ef26c966b97c5d3249f5af
Benjamin Eriksson <benjamin.eriksson@gmail.com>
2012-04-22 21:39:59 +0200

    adds more target specific types

--- src/foobar_target.h
+++ src/foobar_target.h
@@ -6,7 +6,11 @@
 #define IN_BUFFER_SIZE  512
 #define OUT_BUFFER_SIZE 512
 
+typedef intmax_t  target_intmax_t;
+
 typedef intptr_t  target_intptr_t;
 typedef uintptr_t target_uintptr_t;
+typedef int64_t   target_intlargest_t;
+typedef uint64_t  target_uintlargest_t;
 
 #endif
